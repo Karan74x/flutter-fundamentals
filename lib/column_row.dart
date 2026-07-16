cc4a78fcddf44e53e78bbad85a450af6f1e5e6ce
Karan Rahani <karanrahani6@gmail.com>
2026-07-16 20:07:35 +0530

    Complete Block 1 of 3

--- lib/column_row.dart
+++ lib/column_row.dart
@@ -6,31 +6,81 @@ class ColumnRow extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body:Row(
+      body: Column(
         children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Container(
-              height: 50,
-              width: 100,
-              color: Colors.red,
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Container(
-              height: 50,
-              width: 100,
-              color: Colors.blue,
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Container(
-              height: 50,
-              width: 100,
-              color: Colors.green,
-            ),
+          //Block 1
+          Row(
+
+            children: [
+              // PART 1
+              Padding(
+                padding: const EdgeInsets.all(4.0),
+                child: Container(
+                  height: 100,
+                  width: 30,
+                  color: Colors.red,
+                ),
+              ),
+
+              // PART 2
+              Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(4.0),
+                    child: Container(
+                      height: 30,
+                      width: 100,
+                      color: Colors.blue,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(4.0),
+                    child: Container(
+                      height: 30,
+                      width: 100,
+                      color: Colors.blue,
+                    ),
+                  )
+                ],
+              ),
+
+              //PART 3
+              Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(4.0),
+                    child: Container(
+                      height: 50,
+                      width: 50,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(50),
+                        color: Colors.orange,
+                      ),
+                    ),
+                  ),
+                   Padding(
+                     padding: const EdgeInsets.all(4.0),
+                     child: Container(
+                        height: 50,
+                        width: 50,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(50),
+                          color: Colors.orange,
+                        ),
+                      ),
+                   ),
+                ],
+              ),
+              // PART 4
+              Padding(
+                padding: const EdgeInsets.all(4.0),
+                child: Container(
+                  height: 100,
+                  width: 30,
+                  color: Colors.green,
+                ),
+              ),
+            ],
           )
         ],
       )
