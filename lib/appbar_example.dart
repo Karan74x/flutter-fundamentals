0b53eb34fc092826726819534c1bac442af9eb8a
Karan Rahani <karanrahani6@gmail.com>
2026-08-02 11:30:25 +0530

    Renamed practice.dart to appbar_example.dart

---
+++ lib/appbar_example.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class AppBarExample extends StatelessWidget {
+  const AppBarExample({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Color.fromARGB(201, 117, 114, 114),
+        title: Text(
+          "APP BAR EXAMPLE",
+          style: TextStyle(color: Color.fromARGB(255, 210, 62, 33)),
+        ),
+      ),
+      body: Center(
+        child: Text(
+          "This is in body",
+          style: TextStyle(
+            color: Color.fromARGB(200, 58, 5, 232),
+            fontSize: 25,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
+}
